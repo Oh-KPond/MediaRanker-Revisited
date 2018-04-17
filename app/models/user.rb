710724d6b7da8302ba32733698a8baa8238d53e5
Kate Pond <kate@thepondsedge.com>
2018-04-17 16:05:46 -0700

    impliments OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+  def self.build_from_github(auth_data)
+    return User.create(
+      provider: auth_data[:provider],
+      uid: auth_data[:uid],
+      email: auth_data[:info][:email],
+      username: auth_data[:info][:nickname]
+    )
+  end
 end
