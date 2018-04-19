508a685582b68cef5986446a643ee9169261c47f
Kate Pond <kate@thepondsedge.com>
2018-04-18 22:45:49 -0700

    creates authorized and unauthorized pages- re: login vs logged out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
-
   def render_404
     # DPR: this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
   end
 
-private
-  def find_user
+  def require_login
+    unless current_user
+      flash[:status] = :alert
+      flash[:result_text] = "You must be logged in to do that"
+      redirect_to root_path
+    end
+  end
+
+  private
+  def current_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
     end
